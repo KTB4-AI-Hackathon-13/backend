f4f3e9e2c44c94a384b914224275cf6dbd56f3a6
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2026-08-19 20:39:20 +0900

    Revert "feat: 인증/인가"

--- src/main/java/hackathon/app/auth/infrastructure/CryptoConfig.java
+++
@@ -1,9 +0,0 @@
-package hackathon.app.auth.infrastructure;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-@Configuration
-public class CryptoConfig {
-    @Bean PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
-}
